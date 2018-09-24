129237248e54d7c89cd8f3be4ddef50f3dc880ef
SabyGil <sabygil@yahoo.com>
2018-09-24 15:18:40 -0400

    added inital media queries for all views

--- src/components/FullpageParent.js
+++ src/components/FullpageParent.js
@@ -3,7 +3,9 @@ import Menu from './Menu';
 import FullpageWrapper from './Fullpage';
 import $ from 'jquery';
 import GIF from '../imgs/test.gif';
-import { Consumer } from '../context';
+import {
+	Consumer
+} from '../context';
 
 import 'fullpage.js/vendors/scrolloverflow';
 
@@ -21,30 +23,53 @@ export default class FulpageParent extends React.Component {
 
 	componentDidMount() {
 		var pos_test = 0;
-		$( "#logo" ).css({width: "100%"}, 1000);
-		$( "#logo-container" ).css({transform: "translate(-50%, -50%)", top:"50%"}, 1000);
+		$("#logo").css({
+			width: "100%"
+		}, 1000);
+		$("#logo-container").css({
+			transform: "translate(-50%, -50%)",
+			top: "50%"
+		}, 1000);
 
-		$(window).on('hashchange', function(e){
-			if(this.window.location.hash == "#Main"){
-				$( "#logo" ).animate({a: "-50%"},{
-					duration: 500, 
-					step: function(now,fx){
-						console.log(now)
-						$( "#logo-container" ).css({transform: "translate(" + now + "%, -50%)"}, 1000);
+		$(window).on('hashchange', function(e) {
+			if(this.window.location.hash == "#Main") {
+				$("#logo").animate({
+					a: "-50%"
+				}, {
+					duration: 500,
+					step: function(now, fx) {
+						// console.log(now)
+						$("#logo-container").css({
+							transform: "translate(" + now + "%, -50%)"
+						}, 1000);
 					}
 				});
-				$( "#logo" ).animate({width: "100%"},1000);
-				$( "#logo-container" ).animate({animation: "fullLogo 2s", top:"50%"}, 1000);
-			}else{
-				$( "#logo" ).animate({width: "20%"},1000);
-				$( "#logo" ).animate({a: "-100%"},{
-					duration: 500, 
-					step: function(now,fx){
-						console.log(now)
-						$( "#logo-container" ).css({transform: "translate(" + now + "%, -50%)"}, 1000);
+				$("#logo").animate({
+					width: "100%"
+				}, 1000);
+				$("#logo-container").animate({
+					animation: "fullLogo 2s",
+					top: "50%"
+				}, 1000);
+			} else {
+				$("#logo").animate({
+					width: "20%"
+				}, 1000);
+				$("#logo").animate({
+					a: "-100%"
+				}, {
+					duration: 500,
+					step: function(now, fx) {
+						// console.log(now)
+						$("#logo-container").css({
+							transform: "translate(" + now + "%, -50%)"
+						}, 1000);
 					}
 				});
-				$( "#logo-container" ).animate({animation: "fullLogo 2s", top:"10%"}, 1000);
+				$("#logo-container").animate({
+					animation: "fullLogo 2s",
+					top: "10%"
+				}, 1000);
 			}
 		});
 	}
